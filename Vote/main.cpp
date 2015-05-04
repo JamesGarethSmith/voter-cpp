9a7bd422cbf3a05a0330aa3acddd415572d2c235
jamesgarethsmith <james.gareth.smith@gmail.com>
2015-05-04 16:33:12 +0200

    Initial Commit

---
+++ Vote/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Vote
+//
+//  Created by James on 2015/05/04.
+//  Copyright (c) 2015 James. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
